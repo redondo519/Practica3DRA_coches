2e74272c55bb73d20409b7640e9e0523af395bc3
https://github.com/redondo519/PROG_DAVID.git <david.redalo@iesjulianmarias.es>
2025-05-24 18:15:55 +0200

    Comit 2.1: Creacion de Interfaz

--- src/Concesionario.java
+++ src/Concesionario.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class Concesionario {
+    private JLabel Titulo;
+    private JPanel Panel1;
+    private JLabel Modelo;
+    private JComboBox comboModelo;
+    private JButton botonVisualizar;
+    private JLabel Motor;
+    private JComboBox comboMotor;
+    private JLabel Color;
+    private JComboBox comboColor;
+    private JTextArea textArea1;
+    private JLabel Ruedas;
+    private JComboBox comboRuedas;
+    private JLabel PilotoAutomatico;
+    private JComboBox comboPiloto;
+    private JButton botonPedido;
+    private JButton botonSeleccionado;
 }
