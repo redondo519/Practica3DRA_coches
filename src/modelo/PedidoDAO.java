44f3c35176a645f0fd44e06ad601475b91382211
redondo519 <david.redalo@iesjulianmarias.es>
2025-05-26 21:30:10 +0200

    COMIT clase 3.1: Controlador >> vista.getBotonPedido()

--- src/modelo/PedidoDAO.java
+++ src/modelo/PedidoDAO.java
@@ -5,4 +5,8 @@ public class PedidoDAO {    //Clase que guarda los pedidos en la BD
     //Metodo almacenarPedido
     //public static boolean almacenaPedido(datos que voy a meter en el insert)
 
+    public static boolean almacenarPedido(Pedido pedido){
+        return false;
+    }
+
 }
